d5016fc283686cb96646def950507645cf5170cc
Lela12 <dbcocotn4@hanmail.net>
2021-12-12 16:36:15 +0900

    [Finish] 데이터 수정 및 삭제

--- src/components/PhoneInfo.js
+++ src/components/PhoneInfo.js
@@ -9,6 +9,56 @@ class PhoneInfo extends Component {
     },
   };
 
+  state = {
+    editing: false,
+    name: "",
+    phone: "",
+  };
+
+  handleRemove = () => {
+    const { info, onRemove } = this.props;
+    onRemove(info.id);
+  };
+
+  // editing 값을 반전시키는 함수이다
+  // true->false, false->true
+  handleToggleEdit = () => {
+    const { editing } = this.state;
+    this.setState({
+      editing: !editing,
+    });
+  };
+
+  //input에서 onChange 이벤트가 발생 될 때
+  //호출되는 함수이다
+  handleChange = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  componentDidUpdate(preProps, prevState) {
+    //   수정을 눌렀을땐, 기존의 값이 input에 나타나고
+    // 수정을 적용할땐, input의 값들을 부모한테 전달해준다
+
+    const { info, onUpdate } = this.props;
+    if (!prevState.editing && this.state.editing) {
+      //editing값이 false->true로 전환 될 때
+      // info의 값을 state에 넣어준다
+      this.setState({
+        name: info.name,
+        phone: info.phone,
+      });
+    }
+    if (prevState.editing && !this.state.editing) {
+      onUpdate(info.id, {
+        name: this.state.name,
+        phone: this.state.phone,
+      });
+    }
+  }
+
   render() {
     const style = {
       border: "1px solid black",
@@ -16,7 +66,36 @@ class PhoneInfo extends Component {
       margin: "8px",
     };
 
-    const { name, phone, id } = this.props.info;
+    const { editing } = this.state;
+
+    if (editing) {
+      //수정모드
+      return (
+        <div style={style}>
+          <div>
+            <input
+              value={this.state.name}
+              name="name"
+              placeholder="이름"
+              onChange={this.handleChange}
+            />
+          </div>
+          <div>
+            <input
+              value={this.state.phone}
+              name="phone"
+              placeholder="전화번호"
+              onChange={this.handleChange}
+            />
+          </div>
+          <button onClick={this.handleToggleEdit}>적용</button>
+          <button onClick={this.handleRemove}>삭제</button>
+        </div>
+      );
+    }
+
+    //일반모드
+    const { name, phone } = this.props.info;
 
     return (
       <div style={style}>
@@ -24,6 +103,8 @@ class PhoneInfo extends Component {
           <b>{name}</b>
         </div>
         <div>{phone}</div>
+        <button onClick={this.handleToggleEdit}>수정</button>
+        <button onClick={this.handleRemove}>삭제</button>
       </div>
     );
   }
